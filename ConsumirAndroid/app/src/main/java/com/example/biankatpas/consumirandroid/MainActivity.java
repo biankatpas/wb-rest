08566ebf3e87cf5d1fa48459e47f2e4b67c17fad
Bianka Passos <biankatpas@gmail.com>
2018-12-07 18:47:50 -0200

    android consumindo produto tbm

--- ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/MainActivity.java
+++ ConsumirAndroid/app/src/main/java/com/example/biankatpas/consumirandroid/MainActivity.java
@@ -20,22 +20,49 @@ public class MainActivity extends AppCompatActivity {
 
         AcessoRest ar = new AcessoRest();
 
-        String chamadaWS = "http://192.168.0.117:8084/Produto-WS/webresources/generic/usuario/get/melapso";
-        String resultado = ar.exemploGet(chamadaWS);
-
-        Log.i("JSON", resultado);
-
         try
         {
 
             Gson g = new Gson();
             Usuario u = new Usuario();
+            Produto p = new Produto();
 
-            Type usuarioType = new TypeToken<Usuario>() {}.getType();
-            u = g.fromJson(resultado, usuarioType);
+            int type = 2;
 
-            TextView texto = findViewById(R.id.texto);
-            texto.setText(String.format("%s\n%s\n%s\n%s", u.getLogin(), u.getSenha(), u.getEmail(), u.getPerfil()));
+            switch (type)
+            {
+                case 1:
+                    //        String chamadaWS = "http://192.168.0.117:8084/Produto-WS/webresources/generic/usuario/get/melapso";
+                    String chamadaWS = "http://10.7.24.137:8084/Produto-WS/webresources/generic/usuario/get/melapso";
+                    String resultado = ar.exemploGet(chamadaWS);
+                    Log.i("JSON", resultado);
+
+                    Type usuarioType = new TypeToken<Usuario>() {}.getType();
+                    u = g.fromJson(resultado, usuarioType);
+
+                    TextView texto = findViewById(R.id.texto);
+                    texto.setText(String.format("%s\n%s\n%s\n%s", u.getLogin(), u.getSenha(), u.getEmail(), u.getPerfil()));
+
+                    break;
+                case 2:
+
+                    //        String chamadaWS = "http://192.168.0.117:8084/Produto-WS/webresources/generic/usuario/get/melapso";
+                    String chamadaWS2 = "http://10.7.24.137:8084/Produto-WS/webresources/generic/produto/get/tofu";
+                    String resultado2 = ar.exemploGet(chamadaWS2);
+                    Log.i("JSON", resultado2);
+
+                    Type produtoType = new TypeToken<Produto>() {}.getType();
+                    p = g.fromJson(resultado2, produtoType);
+
+                    TextView texto2 = findViewById(R.id.texto);
+                    texto2.setText(String.format("%s\n%s\n%s", p.getDescricao(), p.getVencimento(), p.getVendedor()));
+
+                    break;
+                default:
+
+            }
+
+
 
         }
         catch (Exception ex) { }
